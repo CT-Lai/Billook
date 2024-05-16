b5b939202ad86692b1565262665e8ccd698ee72c
yyp <zseff123@gmail.com>
2024-05-16 17:32:04 +0800

    modified:   billook/package-lock.json 	modified:   billook/package.json 	modified:   billook/public/index.html 	new file:   billook/public/logo.png 	modified:   billook/src/App.js 	new file:   billook/src/Assets/homepg_img.jpg 	new file:   billook/src/Assets/left_quotation_mark.jpg 	new file:   billook/src/Assets/right_quotation_mark.jpg 	new file:   billook/src/Pages/Homepage/CreateBook.jsx 	new file:   billook/src/Pages/Homepage/Homepage.jsx 	new file:   billook/src/Pages/UserBook/BookListitem.jsx 	new file:   billook/src/Pages/UserBook/BooksList.jsx

--- billook/src/App.js
+++ billook/src/App.js
@@ -1,17 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout';
-import BooksList from './Components/BooksList';
+import Homepage from './Pages/Homepage/Homepage';
 
 function App() {
     return (
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<Layout />}>
-                <Route index element={<h1>Home</h1>} />
-                <Route path="mybook" element={<BooksList />} />
-                <Route path="joinbook" element={<h1>join-book</h1>} />
-                <Route path="login" element={<h1>login</h1>} />
-                <Route path="*" element={<h1>Error 404: Not Found</h1>} />
+                <Route index element={<Homepage/>}/>
             </Route>
         </Routes>      
         </BrowserRouter>
